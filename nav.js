b0f69c704df6c1e7db74db2a8dda9e605edc7e22
Dilshan Priyawansha <it21353284@my.slitt.lk>
2023-08-20 21:54:14 +0530

    update

--- nav.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById("menuIcon");
-const navLinks = document.getElementById("navLinks");
-
-menuIcon.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
